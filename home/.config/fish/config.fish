9f08ebab2234065ddd3d5164edbfbd80dfde94d0
dfang <df1228@gmail.com>
2015-11-06 02:34:34 +0800

    Ignore .DS_Store file

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,1 +1,8 @@
-/Users/dfang/Dropbox/Mackup/.config/fish/config.fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH /Users/dfang/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /Users/dfang/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
