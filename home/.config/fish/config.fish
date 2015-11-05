cf6d55a0670305b3618a50b43420a4ca50412640
dfang <df1228@gmail.com>
2015-11-06 02:37:29 +0800

    MISC - Remove fish configuration in favor of omf that managed by homesick

--- home/.config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH /Users/dfang/.local/share/omf
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /Users/dfang/.config/omf
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
