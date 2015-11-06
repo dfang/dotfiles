46f25b5a8b49516a1e405f656dfa91539a8d9aaf
dfang <df1228@gmail.com>
2015-11-06 14:11:19 +0800

    MISC - Update fish dotfiles

--- home/.config/omf/fish_aliases_and_functions.fish
+++ home/.config/omf/fish_aliases_and_functions.fish
@@ -1,5 +1,19 @@
+# use fasd plugin for oh-my-fish
+
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
+alias j=z
+alias jj=zz
+
+
 function da
-  docker start $ ;and docker attach $1
+  docker start $1 ;and docker attach $1
 end
 
 function newbox
