f7de0616e0370030f2ae6c1a376b7b7c2c6a8582
dfang <df1228@gmail.com>
2015-11-08 13:13:58 +0800

    MISC - Update dotfiles

--- home/.config/omf/fish_aliases_and_functions.fish
+++ home/.config/omf/fish_aliases_and_functions.fish
@@ -8,10 +8,8 @@ alias d='fasd -d'        # directory
 alias f='fasd -f'        # file
 alias sd='fasd -sid'     # interactive directory selection
 alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i' # cd with interactive selection
-alias j=z
-alias jj=zz
+alias z=j
+
 
 alias gfl="git fetch ;and git lg" # Git fetch and show log
 
