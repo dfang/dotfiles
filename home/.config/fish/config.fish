5e115b8d3ac83cccb76b00a59acfdd31769d23bc
dfang <df1228@gmail.com>
2015-11-22 23:13:20 +0800

    MISC

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,20 +7,3 @@ set -gx OMF_CONFIG ~/.config/omf
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
-
-if test -e ~/.aliases
-  source ~/.aliases
-end
-
-
-# Prepend a path to $PATH
-set -x PATH ~/bin $PATH
-set -x PATH ~/.bin $PATH
-set -x PATH ./bin $PATH
-
-# comment out here because oh my fish rbenv plugin support this
-# set PATH /usr/local/bin $PATH
-# set PATH ~/.rbenv/shims $PATH
-
-set PATH $HOME/.meteor $PATH
-set -gx NVM_DIR (brew --prefix nvm)
--- home/.config/omf/init.fish
+++ home/.config/omf/init.fish
@@ -1,3 +1,22 @@
 if test -e $OMF_CONFIG/fish_aliases_and_functions.fish
   source $OMF_CONFIG/fish_aliases_and_functions.fish
 end
+
+if test -e ~/.aliases
+  source ~/.aliases
+end
+
+set PATH $HOME/.meteor $PATH
+
+# Prepend a path to $PATH
+set -x PATH ~/bin $PATH
+set -x PATH ~/.bin $PATH
+set -x PATH ./bin $PATH
+
+
+# comment out here because oh my fish rbenv plugin support this
+# set PATH /usr/local/bin $PATH
+# set PATH ~/.rbenv/shims $PATH
+# set -gx NVM_DIR (brew --prefix nvm)
+
+# set -gx PATH /usr/local/bin $PATH
